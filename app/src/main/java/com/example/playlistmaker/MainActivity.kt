f1373f51d456782eee8a9e6c7e6b5adf98d894b9
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2024-06-16 01:27:54 +0300

    HW_8/4/2. Настроены обработчики нажатия кнопок: Поиск, Медиатека, Настройки.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Обработка нажатия кнопки Поиск через способ 1 - реализацию абстрактного класса
+        val searchButton = findViewById<Button>(R.id.search)
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Запустили поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        // Обработка нажатия кнопки Медиатека через способ 2 - лямбда
+        val mediaButton = findViewById<Button>(R.id.media)
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Запустили медиатеку!", Toast.LENGTH_SHORT).show()
+        }
+
+        // Обработка нажатия кнопки Настройки через способ 2 - лямбда
+        val settingsButton = findViewById<Button>(R.id.settings)
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Запустили настройки!", Toast.LENGTH_SHORT).show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
