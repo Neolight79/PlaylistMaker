e89e27927fd9d6ffe45849b1dd6bcd7b4c56bc96
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2024-06-17 00:57:34 +0300

    HW_8/5/6. Добавлены активити для Поиска и Медиатеки, настроена навигация для экранов Поиск, Медиатека, Настройки. (UPD #2) Заменил кустарный тулбар на системный в активити настроек.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
