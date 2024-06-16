5f515b82e34974cf7b04cb6f645e72a1cefed622
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2024-06-16 16:13:50 +0300

    HW_8/5/6. Добавлены активити для Поиска и Медиатеки, настроена навигация для экранов Поиск, Медиатека, Настройки.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,7 +3,6 @@ package com.example.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
