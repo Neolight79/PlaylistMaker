de117952318bf1653a6dee98e44517843fe80637
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2024-07-14 00:43:53 +0300

    Изменения согласно заданию спринта №10: - Настроен RecyclerView для списка найденных треков - Настроено получение изображений по ссылкам через Glide - Интерфейс доработан в соответствии с требованиями макета в Figma - Настроен механизм ограничения длины названий треков и испонителя

---
+++ app/src/main/java/com/example/playlistmaker/SearchListAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchListAdapter(private val foundTracks: ArrayList<Track>) : RecyclerView.Adapter<SearchListViewHolder> () {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_search_item, parent, false)
+        return SearchListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SearchListViewHolder, position: Int) {
+        holder.bind(foundTracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return foundTracks.size
+    }
+}
