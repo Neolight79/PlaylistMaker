ebc49cab6278c79a900a0dbe0925d4574208a4ed
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2025-02-23 23:11:08 +0300

    Изменения согласно заданию спринта №23: - На базе предоставленного макета реализована адаптивная иконка приложения. - Верстка экранов и элементов интерфейса приложения адаптирована к требованиям дизайна. - Реализован экран отображения и работы с плейлистом, настроены операции навигации. - Добавлены механизмы отображения статистики по количеству минут и количеству треков в плейлисте. - В связи с переиспользованием объектов для RecyclerView избранных треков, переименованы модули вместо префикса "Favorite" на префикс "Tracks". - Реализована функция "Поделиться" для плейлиста с формированием содержимого в текстовом виде. - Реализован механизм удаления плейлиста и всех орфанных треков в таблице треков в плейлистах. - Реализован механизм редактирования данных плейлиста при помощи наследования объектов экрана создания плейлиста. - Проведено тестирование приложения, подтверждены целевые характеристики и функционал.

--- app/src/main/java/com/example/playlistmaker/main/ui/activity/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/activity/RootActivity.kt
@@ -33,6 +33,7 @@ class RootActivity : AppCompatActivity() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.createPlaylistFragment -> binding.bottomNavigationView.isVisible = false
+                R.id.editPlaylistFragment -> binding.bottomNavigationView.isVisible = false
                 R.id.playlistFragment -> binding.bottomNavigationView.isVisible = false
                 R.id.playerFragment -> binding.bottomNavigationView.isVisible = false
                 else -> binding.bottomNavigationView.isVisible = true
